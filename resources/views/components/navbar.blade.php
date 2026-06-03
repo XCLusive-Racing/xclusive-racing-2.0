2b3ec46573934d3c36a394afbdb2a761d70d1aa4
DeEchteOlle <ollekuiper07@gmail.com>
2026-06-03 15:08:41 +0200

    Update navbar; replace 'HOTLAPS' with 'LEADERBOARD' in dropdown menu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -58,13 +58,7 @@
                         <li>
                             <a class="xcl-dropdown-item" href="{{ route('drivers.index') }}">
                                 <svg width="14" height="14" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/></svg>
-                                RESULTS
-                            </a>
-                        </li>
-                        <li>
-                            <a class="xcl-dropdown-item" href="{{ route('hotlaps.index') }}">
-                                <svg width="14" height="14" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>
-                                HOTLAPS
+                                LEADERBOARD
                             </a>
                         </li>
                     </ul>
